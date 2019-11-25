b9bf5f3a5bd44a1058d6b2ceb19f2520cc9fa229
Abhijeet Sinha <abhisinha4395@MacBook-Air-4.local>
2019-11-25 12:50:45 +0530

    Bug fixes

--- social_media/posts/views.py
+++ social_media/posts/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.http import Http404
 from braces.views import SelectRelatedMixin
+from django.contrib import messages
 
 from .import models
 from . import forms
@@ -15,7 +16,7 @@ User = get_user_model()
 
 class PostList(SelectRelatedMixin, generic.ListView):
     model = models.Post
-    select_related = (user, group)
+    select_related = ('user', 'group')
 
 class UserPosts(generic.ListView):
     model = models.Post
@@ -23,7 +24,7 @@ class UserPosts(generic.ListView):
 
     def get_queryset(self):
         try:
-            self.post.user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+            self.post_user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
         except User.DoesNotExist:
             raise Http404
         else:
